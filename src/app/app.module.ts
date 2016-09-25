4e630c09570f15a6513e62d991cd905d30e6315b
Darren King <darrenbrettking@hotmail.com>
2016-09-25 12:27:10 -0700

    Added API service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
+import { HackerNewsAPIService } from './hackernews-api.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { ItemComponent } from './item/item.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ HackerNewsAPIService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
