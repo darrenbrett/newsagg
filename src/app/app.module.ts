b2aa8d3bdcd8d38440a7747e30821af8bbddb873
Darren King <darrenbrettking@hotmail.com>
2016-09-25 13:39:05 -0700

    Added routing and itemcomments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +10,7 @@ import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
 import { HackerNewsAPIService } from './hackernews-api.service';
+import { ItemCommentsComponent } from './itemcomments/itemcomments.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,14 @@ import { HackerNewsAPIService } from './hackernews-api.service';
     HeaderComponent,
     StoriesComponent,
     FooterComponent,
-    ItemComponent
+    ItemComponent,
+    ItemCommentsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [ HackerNewsAPIService ],
   bootstrap: [AppComponent]
---
+++ src/app/itemcomments/itemcomments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itemcomments',
+  templateUrl: './itemcomments.component.html',
+  styleUrls: ['./itemcomments.component.scss']
+})
+export class ItemCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
